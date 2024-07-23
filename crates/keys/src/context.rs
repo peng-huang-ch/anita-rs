a4934bda6f8ea91825b7cc019a4924902812793d
peng-huang-ch <h499871809@gmail.com>
2024-07-23 15:51:04 +0800

    doc: function comments

--- crates/keys/src/context.rs
+++ crates/keys/src/context.rs
@@ -6,7 +6,9 @@ pub struct KeypairContext {
     pub chain: Chain,
 }
 
+/// A context for generating and signing keypairs.
 impl KeypairContext {
+    /// Create a new keypair context.
     pub fn from_chain(chain: Chain) -> Self {
         let strategy: Box<dyn KeypairStrategy> = match chain {
             Chain::SOLANA => Box::new(SolanaKeyPair),
@@ -15,10 +17,12 @@ impl KeypairContext {
         KeypairContext { strategy, chain }
     }
 
+    /// Generate a new keypair.
     pub fn generate_keypair(&self) -> Keypairs {
         self.strategy.generate_keypair()
     }
 
+    /// Sign a message with a secret key.
     pub fn sign(&self, secret: &str, message: &str) -> String {
         let message = message.as_bytes();
         self.strategy.sign(secret, message)
